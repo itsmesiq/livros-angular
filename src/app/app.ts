0f1fdaa30dc44140f5187449fb1c2662be5615dc
Ana <anasiqueira.serpentis@gmail.com>
2026-01-31 21:52:32 -0300

    Add book form implementation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { LivroLista } from "./livro-lista/livro-lista";
+import { RouterOutlet, RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-root',
-  imports: [LivroLista],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/livro-dados/livro-dados.ts
+++ src/app/livro-dados/livro-dados.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Livro } from '../livro';
+import { Editora } from '../editora';
+import { ControleEditoraService } from '../controle-editora.service';
+import { ControleLivrosService } from '../controle-livros.service';
 
 @Component({
   selector: 'app-livro-dados',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './livro-dados.html',
   styleUrl: './livro-dados.css',
 })
-export class LivroDados {
+export class LivroDados implements OnInit {
+  public livro: Livro = new Livro();
+  public autoresForm: string = "";
+  public editoras: Array<Editora> = [];
 
+  private servEditora: ControleEditoraService;
+  private servLivros: ControleLivrosService;
+  private router: Router;
+
+  constructor(servEditora: ControleEditoraService, servLivros: ControleLivrosService, router: Router){
+    this.servEditora = servEditora;
+    this.servLivros = servLivros;
+    this.router = router;
+  }
+
+  ngOnInit(): void {
+    this.editoras = this.servEditora.getEditoras();
+  }
+
+  incluir = () => {
+    this.livro.autores = this.autoresForm.split('\n');
+    this.servLivros.incluir(this.livro);
+    this.router.navigateByUrl('/lista');
+  }
 }
